be82478530dd3ce87bcdaea8a54e85bad1518b67
Mohamed Sebri <127107517+MedSebri@users.noreply.github.com>
2024-02-20 05:48:13 +1300

    Update Exti.c

--- Exti.c
+++ Exti.c
@@ -5,7 +5,7 @@ void pa0_exti_init() {
 	RCC->AHB1ENR |=(1<<0);  //enable clock access for GPIOA
 	RCC->APB2ENR |=(1<<14); //enable clock for syscfg
 	//configure PA0 as input : Done by default
-	//select portA for EXTI0 : Done by default : check SYSCFG_EXTICR1_
+	//select portA for EXTI0 : Done by default : check SYSCFG_EXTICR1
 	EXTI->IMR  |=(1<<0);    //unmask EXTI0
 	EXTI->FTSR |=(1<<0);    //select risng edge
 	NVIC_EnableIRQ(EXTI0_IRQn);      //enable EXTI line in NVIC
